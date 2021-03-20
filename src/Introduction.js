af680fbc865f982cb00e4d3a06c3774c79b0b50d
Illari Alvarez-Gil Escobar <illari.alvarezgil@gmail.com>
2021-03-20 00:06:32 -0500

    NavBar desktop

--- src/Introduction.js
+++ src/Introduction.js
@@ -3,13 +3,9 @@ function Introduction(params) {
     return (
         <div className='IntroductionText'>
             <h1>Welcome to Zen for Change, a private practice in Fells Point-Baltimore</h1>
-            <p>We are a mental health and sex therapy practice with a special focus on trauma, culture and sexuality. 
-Finding a place we can trust to tell our stories is essential to finding a path to healing, recovery, and transformation. 
-We all are born with an innate capacity to develop happy and healthy lives. However, sometimes life experiences such as hurts, and traumas can interfere. We may become unhappy or unsatisfied with our lives and adopt self-defeating thinking and behavioral patterns.
-
- We are here to assist you with your needs and collaboratively work to achieve your goals towards your well-being.</p>
+            <p>We are a mental health and sex therapy practice with a special focus on culture, diversity and trauma. We understand that finding a place we can trust to tell our stories is essential to finding a path to healing, recovery, and transformation. We are here to assist you with your needs and collaboratively work towards your goals.</p>
         </div>
     )
-    
+
 }
 export default Introduction
--- src/navBar.js
+++ src/navBar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import headerOption1 from './img/headerOption1.png'
 function NavBar(params) {
   return (
+    <div>
       <div className='NavBar'>
         <img src={headerOption1} alt='header'></img>
         <div className='routes'>
@@ -10,8 +11,13 @@ function NavBar(params) {
           <a href='/sextherapy'> Sex Therapy</a>
           <a href='/forms'>Forms</a>
         </div>
-       </div>
+      </div>
+      <div className='languages'>
+        <a href='/aboutus'>English</a>
+        <a href='/psychotherapy'>Español</a>
+      </div>
+    </div>
   )
-  
+
 }
 export default NavBar
