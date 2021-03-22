b28835cfb209c0deb01fa4052edea1ff7d381cf1
Illari Alvarez-Gil Escobar <illari.alvarezgil@gmail.com>
2021-03-22 10:09:20 -0500

    footer

--- src/footer.js
+++ src/footer.js
@@ -3,7 +3,12 @@ function Footer(params) {
     return (
         <div className='Footer'>
             <footer>
-                <p>Zen for Change, LLC • Baltimore, MD • 443.326.9018 • info@zenforchange.com</p>
+                    <a href='https://www.facebook.com/zenforchange/'><span class="iconify" data-icon="ant-design:facebook-filled" data-inline="false"></span></a>
+                    <a href='https://www.instagram.com/zen_for_change/'><span class="iconify" data-icon="ant-design:instagram-filled" data-inline="false"></span></a>
+                    <a href='https://www.linkedin.com/company/zen-for-change-llc/'><span class="iconify" data-icon="akar-icons:linkedin-fill" data-inline="false"></span></a>
+                    <a href='https://twitter.com/niasimich'><span class="iconify" data-icon="akar-icons:twitter-fill" data-inline="false"></span></a>
+                    <p>Zen for Change, LLC • Baltimore, MD • 443.326.9018 • info@zenforchange.com</p>
+            
             </footer>
         </div>
     )
