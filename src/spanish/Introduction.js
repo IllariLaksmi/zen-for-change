361cf340b7f6e02504db573cfe528ed4948dbda3
Illari Alvarez-Gil Escobar <illari.alvarezgil@gmail.com>
2021-05-09 16:35:32 -0500

    traduccion

--- src/spanish/Introduction.js
+++ src/spanish/Introduction.js
@@ -9,7 +9,7 @@ function Introduction(params) {
                 <img className='introImage' src={FrontPage}></img>
                 <div className ='introText'>
                 <h1 >Bienvenido a Zen for Change</h1>
-                <p><b>Somos una práctica privada que brinda servicios de salud mental y terapia sexual. Encontrar un lugar en el cual uno se sienta con confianza suficiente para contar nuestras historias es indispensable para seguir el camino de la sanación emocional y la transformación.Un camino desconocido solo nos es desconocido hasta que estemos listos para explorarlo. Estamos aquí para asistirlo con sus necesidades y juntos trabajar para lograr sus objetivos hacia su bienestar.</b></p>
+                <p>Somos una práctica privada que brinda servicios de salud mental y terapia sexual. Encontrar un lugar en el cual uno se sienta con confianza suficiente para contar nuestras historias es indispensable para seguir el camino de la sanación emocional y la transformación.Un camino desconocido solo nos es desconocido hasta que estemos listos para explorarlo. Estamos aquí para asistirlo con sus necesidades y juntos trabajar para lograr sus objetivos hacia su bienestar.</p>
                 </div>
             </div>
             <div className ='whyText'>               
